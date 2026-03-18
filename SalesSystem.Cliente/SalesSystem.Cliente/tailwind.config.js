1e516f3fdaadeb225502c9eb51aee22587758799
Diana Saico <dianasaico2024@gmail.com>
2026-03-17 22:29:14 -0500

    Ajustes

---
+++ SalesSystem.Cliente/SalesSystem.Cliente/tailwind.config.js
@@ -0,0 +1,11 @@
+﻿/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./**/*.{razor,html,cshtml}",
+        "../SalesSystem.Cliente.Client/**/*.razor"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
